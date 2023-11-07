d8de853c607bde1688d70254413d1d2cdcce5fd4
bilalmuhammad41 <bilalmohammad41@gmail.com>
2023-11-07 22:05:45 +0500

    CardDeal section completed.

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import styles, { layout } from '../style'
+import Button from './Button'
+import {card} from '../assets'
+
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>  
+        <h2 className={`${styles.heading2}`}>Find a better card deal <br className='sm:block hidden'/>
+          in few easy steps</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>We've been researching and comparing credit cards for over 15 years. You can easily compare more than 270 Australian credit cards using our free tools. When you find one that suits, we'll take you securely to the bank's website.</p>
+        
+        <Button styles={`mt-10`}/>
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className='w-[100%] h-[100%]' />
+      </div>
+    </section>
   )
 }
 

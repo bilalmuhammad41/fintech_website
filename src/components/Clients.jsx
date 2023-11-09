753ecf3807475fa1092434abcc6998ce214d8bf5
bilalmuhammad41 <bilalmohammad41@gmail.com>
2023-11-09 13:42:05 +0500

    Testimonials and Clients section completed.

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import styles from '../style'
+import { layout } from '../style'
+import { clients } from '../constants'
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client, index)=>(
+          <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+            <img className='sm:w-[192px] w-[100px] object-contain' src={client.logo} alt='client'/>
+          </div>
+      
+      ))}
+      </div>
+    </section>
   )
 }
 
